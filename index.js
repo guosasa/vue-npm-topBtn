4d5dd8e731c07b8bb33258611043e7aac451d18b
guoss <1060341755@qq.com>
2021-03-04 21:03:59 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+import TopBtn from '../dist/index.js'
+
+new Vue({
+  el: '#app',
+  data () {
+    return {
+
+    }
+  },
+  components: {
+    'top-btn': TopBtn
+  },
+  methods: {},
+  mounted () {
+
+  }
+})
